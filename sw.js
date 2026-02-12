1c12e5f443f12b7cfcc0bb1a934909514931da30
RHLOS <raimon@rhlconsultancy.nl>
2026-02-12 12:22:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service Worker for RHLOS PWA
 // ============================================================
 
-const CACHE_NAME = 'rhlos-v4';
+const CACHE_NAME = 'rhlos-v6';
 const OFFLINE_URL = './';
 
 // Files to cache for offline use (relative paths for GitHub Pages compatibility)
